ad19c1e80dfed6d5aa51e260c4da161ab22a05cf
Aklile Ansa <aklilemente72@gmail.com>
2025-04-23 20:01:38 +0300

    created home page

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='bg-[#f1e6e6a1] z-20 shadow-md shadow-[#968e8e] h-[50px] md:h-[100px]'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
