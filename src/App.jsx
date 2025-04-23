a43079a1a109a868e773b29694d805784fb2a1bd
Aklile Ansa <aklilemente72@gmail.com>
2025-04-23 19:44:59 +0300

    Home page is created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Home/Home'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='bg-[#09090B]'>
+      <Home/>
+    </div>
   )
 }
 
