4270b2d566a3e49262132766ef49ebb72f8cd4a1
Aklile Ansa <aklilemente72@gmail.com>
2025-04-27 22:36:08 +0300

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Components/Home/Home'
 import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
   return (
-    <>
+    <div className='flex flex-col h-[100vh]'>
       <Header/>
       <Home/>
-    </>
+      <div className=''>
+        <Footer/>
+      </div>
+
+    </div>
   )
 }
 
