2b36e87bcab1be45e96f402795ee2e26c0339789
Aklile Ansa <aklilemente72@gmail.com>
2025-04-27 23:21:21 +0300

    course page initalized

---
+++ src/Components/CousesPage/CoursesPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+const CoursesPage = () => {
+  return (
+    <>
+      <Header/>
+
+
+
+
+      <Footer/> 
+    </>
+  )
+}
+
+export default CoursesPage
