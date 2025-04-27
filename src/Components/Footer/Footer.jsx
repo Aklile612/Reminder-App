694d52a5c25a82042dbfd78dc1e9761ae1548303
Aklile Ansa <aklilemente72@gmail.com>
2025-04-27 22:38:39 +0300

    created footer page and done

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <div className='bg-[#1D0B80] flex justify-center'>
+        <span className='text-white font-bold '>Copy Right @2025.  All Right Reserved   </span>
+     </div> 
+    </>
+  )
+}
+
+export default Footer
