d7c85ad011d3349f1646a12b36c09ca3268c40ff
Aklile Ansa <aklilemente72@gmail.com>
2025-04-27 23:20:24 +0300

    modified home page to landing page

--- src/Components/Home/LandingPage.jsx
+++ src/Components/Home/LandingPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import EventCard from '../EventCard/EventCard'
+import SideBar from '../SideBar/SideBar'
 
 const LandingPage = () => {
   return (
-    <div className='mt-[140px] ml-[100px]'>
+    <div className=' flex  '>
+      <div className=''>
+        <SideBar/>
+      </div>
+      <div className='mt-[140px] ml-[100px]'>
         <EventCard/>
+      </div>
     </div>
   )
 }
