a13857c16ffd940256cd98cfe83bbcd0d048062d
Aklile Ansa <aklilemente72@gmail.com>
2025-04-28 21:44:22 +0300

    finished side Bar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { FaRegFolderOpen, FaRobot } from 'react-icons/fa'
 import { IoHomeOutline } from 'react-icons/io5'
+import { Link } from 'react-router-dom'
 
 const SideBar = () => {
   return (
     <div className='w-[300px]   h-[100vh] bg-[#221f1fe3]'>
         <div>
             <div className='flex flex-col md:gap-[50px] justify-center items-center md:pt-[130px]'>
-                <div className=' flex group hover:bg-gray-600 hover:w-[200px] hover:rounded-lg hover:scale-110 hover:text-white  -ml-5 gap-3 transition-all'>
+                <Link className=' flex group hover:bg-gray-600 hover:w-[200px] hover:rounded-lg hover:scale-110 hover:text-white  -ml-5 gap-3 transition-all'>
                     <IoHomeOutline className='text-2xl   '/>
                     <span className='text-sm font-bold font-poppins text-gray-500'>HOME</span>
-                </div>
-                <div className='group flex gap-3 transition-colors '>
+                </Link>
+                <Link className='group flex gap-3 transition-colors '>
                     <FaRegFolderOpen className=' text-[26px]'/>
                     <span className=' text-sm font-bold text-white'>COURSES</span>
-                </div>
+                </Link>
 
-                <div className='group  ml-1.5 flex gap-3 transition-colors '>
+                <Link className='group  ml-1.5 flex gap-3 transition-colors '>
                   <FaRobot className='text-[26px]  ' />
                   <span className=' text-sm font-bold text-white'>Ai Assistant</span>
-                </div>                
+                </Link>                
             </div>
         </div>
     </div>
