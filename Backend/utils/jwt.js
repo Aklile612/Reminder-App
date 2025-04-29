962656641c38048bf8329112b1b2677ee4113c89
Aklile Ansa <aklilemente72@gmail.com>
2025-04-30 02:12:15 +0300

    edited the parameter

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const generateToken=(user)=>{
-    const token = jwt.sign({_id:User._id},process.env.SECREAT_KEY);
+    const token = jwt.sign({_id:user._id},process.env.SECREAT_KEY);
 
     return token;
 }
