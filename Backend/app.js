b43df4a26ec342c39425ac206240a112541ba54b
Aklile Ansa <aklilemente72@gmail.com>
2025-04-30 02:06:22 +0300

    created the route and added

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,12 @@
 import express from "express"
+import authRoute from "./Routes/authRoute.js"
+
+
 const app=express()
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
 
 
-
-
+app.use("/auth",authRoute)
 export default app
