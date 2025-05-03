74838d40c7473cd5c3a5c21b2f5822747b35197c
Aklile Ansa <aklilemente72@gmail.com>
2025-05-03 23:27:17 +0300

    added new find departmet route

--- Backend/controller/courseController.js
+++ Backend/controller/courseController.js
@@ -39,10 +39,46 @@ const addCourse= async (req,res)=>{
     }
 
 }
+//find department
+
+const findDepartment= async  (req,res)=>{
+    const {department_id}= req.params;
+    if (!department_id) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: "Department ID is required" });
+    }
+
+    try {
+        // Find department by _id
+        const department = await Department.findById(department_id);
+
+        return res.status(StatusCodes.OK).json({ department });
+    } catch (error) {
+        console.log(error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Error finding department by ID" });
+    }
+
+}
 //find course by department
 
 const findcourse=async (req,res)=>{
 
-    const {}=req.param
+    const {department_id}=req.params;
+
+    if (!department_id){
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: "Department name is required" });
+    }
+    try {
+        const courses=await Course.find({department:department_id})
+
+        if(!courses||courses.length===0){
+            return res.status(StatusCodes.BAD_REQUEST).json({message:"No course exist in this department!"})
+        }
+        return res.status(StatusCodes.OK).json({message:"Courses Found",courses})
+        
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:"there is some issue with the server!"})
+    }
 }
-export default addCourse;
+export  {addCourse,findDepartment};
