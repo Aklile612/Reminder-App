cf70faccad5d234c32050b27e5e769a1ff4b2fec
Aklile Ansa <aklilemente72@gmail.com>
2025-05-03 22:53:15 +0300

    edited the controller

--- Backend/controller/courseController.js
+++ Backend/controller/courseController.js
@@ -1,23 +1,35 @@
 import {StatusCodes} from "http-status-codes"
 import Course from "../Model/courseModel.js"
+import Department from "../Model/departmentModel.js";
 
+
+
+//add course
 const addCourse= async (req,res)=>{
     const {department_name,coursename }=req.body;
     
-    if (!department_name || !coursename){
+    if (!department_name || !coursename ){
         return res.status(StatusCodes.BAD_REQUEST).json({message:"please fill all the fields"})
     }
 
     try {
+        let department = await Department.findOne({name:department_name})
 
-        const findcourse= await Course.findOne({department_name,coursename})
+        if (!department){
+            department= new Department({
+                name:department_name
+            });
+            await department.save();
+        }
+
+        const findcourse= await Course.findOne({department:department._id,coursename})
 
         if (findcourse) {
             return res.status(StatusCodes.CONFLICT).json({ message: "Course already exists in this department" });
           }
         const addNewDepartmentandCourse= new Course({
             coursename:coursename,
-            department_name:department_name
+            department:department._id,
         })
         const course= await addNewDepartmentandCourse.save()
         return res.status(StatusCodes.ACCEPTED).json({message:"saved the department and course",course})
@@ -26,5 +38,11 @@ const addCourse= async (req,res)=>{
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:"there is some issue with the server!"})
     }
 
+}
+//find course by department
+
+const findcourse=async (req,res)=>{
+
+    const {}=req.param
 }
 export default addCourse;
