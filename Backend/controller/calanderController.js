2d414dd3cc6e33ce281102b2f4a9571364778691
Aklile Ansa <aklilemente72@gmail.com>
2025-05-04 01:55:58 +0300

    edited

--- Backend/controller/calanderController.js
+++ Backend/controller/calanderController.js
@@ -1,16 +1,20 @@
 import {StatusCodes} from "http-status-codes"
 import Calendar from "../Model/calander.module.js";
 
+
 //to add calander
 const addCalander=async (req,res)=>{
     const {topic,date,remindertime,quiz,assignment,mid,final}=req.body;
     const {course_id}=req.params;
+
+
 
     if (!topic || !date || !remindertime){
         return res.status(StatusCodes.BAD_REQUEST).json({message:"please fill all the fields"})
     }
 
     try {
+        
         const newCalander= new Calendar({
             topic:topic,
             date:date,
@@ -30,4 +34,18 @@ const addCalander=async (req,res)=>{
     }
 
 }
-export {addCalander}
+
+// show all events
+const allevents=async (req,res)=>{
+    try {
+        const findEvents= await Calendar.find().sort({ date: 1 }).populate('course', 'name');
+        if (!findEvents || findEvents===0){
+            return res.status(StatusCodes.OK).json({message:"No recent calanders"})
+        }
+        return res.status(StatusCodes.OK).json({ findEvents });
+    } catch (error) {
+        console.log(error)
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:"There was an issue fetching the calendar!"})
+    }
+}
+export {addCalander,allevents}
