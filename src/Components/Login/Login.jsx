fb12837c928087e2c98b43c533697da100bc6b0c
Aklile Ansa <aklilemente72@gmail.com>
2025-05-05 02:15:09 +0300

    edited the login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { AiTwotoneQuestionCircle } from 'react-icons/ai'
 import { FaUser } from 'react-icons/fa'
+import { TfiEmail } from 'react-icons/tfi'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div className='bg-[#1F2937] h-[100vh] w-[100vw] flex justify-center items-center'>
-      <div className='flex flex-col  w-[26vw] h-[70vh] border-amber-50 border-2'>
-        <div className='flex flex-col justify-center items-center gap-3 mb-10'>
+    <div className='bg-[#111827] h-[100vh] w-[100vw] flex justify-center items-center'>
+      <div className='flex flex-col  w-[26vw] h-[80vh] bg-[#1F2937] border-amber-50 '>
+        <div className='flex flex-col justify-center items-center gap-2 mb-10'>
           <span className='text-white md:pt-5 text-3xl'>Register</span>
           <span className='text-white text-sm'>Create Your Account</span>
         </div>
@@ -16,7 +18,7 @@ const Login = () => {
             <span className='ml-5 text-white text-sm md:mb-2'>Username</span>
             <div className='relative'>
                 <input 
-                className='border-gray-700 border-[1px] w-[300px] h-10 ml-5 pl-10 text-white placeholder-text-gray-500 placeholder:text-xs' 
+                className='border-gray-700 border-[1px] w-[300px] h-10 ml-5 pl-10 text-white placeholder-text-gray-500 placeholder:text-xs placeholder:font-bold' 
                 type="email"
                 name='email'
                 placeholder="Enter username"
@@ -24,27 +26,35 @@ const Login = () => {
                 <FaUser className="absolute left-7 top-1/2 transform -translate-y-1/2 text-gray-500" /> 
             </div>
         </div>
-          <div>
-            <input 
-              className='border-gray-700 border-[1px] w-[500px] h-10 pl-4 pt-2'
-              type="email"
-              name='email'
-              placeholder='Email address'
-            />
-          </div>
-
-          <div>
-            <input 
-              className='border-gray-700 border-[1px] w-[500px] h-10 pl-4 pt-2'
-              type="password"
-              placeholder='Password'
-            />
-          </div>
+        <div className='flex flex-col'>
+            <span className='ml-5 text-white text-sm md:mb-2'>Email</span>
+            <div className='relative'>
+                <input 
+                className='border-gray-700 border-[1px] w-[300px] h-10 ml-5 pl-10 text-white placeholder-text-gray-500 placeholder:text-xs placeholder:font-bold' 
+                type="email"
+                name='email'
+                placeholder="Enter Email"
+                />
+                <TfiEmail className="absolute left-7 top-1/2 transform -translate-y-1/2 text-gray-500" /> 
+            </div>
+        </div>
+        <div className='flex flex-col'>
+            <span className='ml-5 text-white text-sm md:mb-2'>Password</span>
+            <div className='relative'>
+                <input 
+                className='border-gray-700 border-[1px] w-[300px] h-10 ml-5 pl-10 text-white placeholder-text-gray-500 placeholder:text-xs' 
+                type="email"
+                name='email'
+                placeholder="Enter Password"
+                />
+                <AiTwotoneQuestionCircle className="absolute left-7 top-1/2 transform -translate-y-1/2 text-gray-500" /> 
+            </div>
+        </div>
 
           <Link 
             to='/'
-            className='ml-50 group hover:scale-110 ease-out transition-all w-[80px] h-12 bg-red-600 text-center pt-3 text-sm text-white font-semibold'>
-            <span>LOGIN</span>
+            className='ml-5 flex justify-center items-center group hover:scale-110 ease-out transition-all w-[300px] h-12 bg-gray-500 text-center  text-sm text-white font-semibold rounded-[6px]'>
+            <span>Create Account</span>
           </Link>
         </form>
       </div>
