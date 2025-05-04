e250151df1a90101b6b57e547dfe0d6c8be7d874
Aklile Ansa <aklilemente72@gmail.com>
2025-05-05 00:49:56 +0300

    created the get started stay organized part

--- src/Components/Get Started/GetStarted.jsx
+++ src/Components/Get Started/GetStarted.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import Header from "../Header/Header.jsx"
+import Footer from "../Footer/Footer.jsx"
 const GetStarted = () => {
   return (
-    <>
-      
-    </>
+    <div>
+        <Header/>
+        {/* stay organized */}
+      <div className='md:ml-[25vw] md:mt-[10vh] flex flex-col md:gap-5'>
+        <span className='text-5xl text-[#1F2937]'>Stay Organized with Best Reminders</span>
+        <span className='max-w-[60vw] text-[#1F2937] font-bold md:ml-[20px] break-words'>Ever tired of missing an assignment a test and when you wake up it is the DeadLine.It won't happen again!!.Never miss an important schedule in our platform organize tests , quizes,assignmentes,   mid exam....</span>
+      </div>
+
+        {/* get strted button */}
+        <div>
+
+        </div>
+
+
+        {/* features */}
+        <div>
+
+        </div>
+
+        <Footer/>
+    </div>
   )
 }
 
