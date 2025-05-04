bd351cb9f9844b503a4ee92faad4221429160797
Aklile Ansa <aklilemente72@gmail.com>
2025-05-04 19:24:19 +0300

    added the route

--- Backend/Routes/calanderRoute.js
+++ Backend/Routes/calanderRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addCalander, allevents } from "../controller/calanderController.js";
+import { addCalander, allevents, singleEventByCalander } from "../controller/calanderController.js";
 
 
 const router= Router()
 
 router.post("/addcalander/:course_id",addCalander);
 router.get("/allcalanders",allevents)
+router.get("/singlecalanderdetail/:calander_id",singleEventByCalander)
 
 export default router
