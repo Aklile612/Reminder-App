469ce807e93145fc3109f4a7b652da37c02da111
Aklile Ansa <aklilemente72@gmail.com>
2025-05-06 00:31:29 +0300

    edited it as best as I can

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { MdOutlineAccessTimeFilled } from 'react-icons/md'
+import { BiSolidEdit } from 'react-icons/bi'
+import { GrFormNextLink } from 'react-icons/gr'
+import { MdDelete, MdOutlineAccessTimeFilled } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
 const EventCard = () => {
   return (
-    <div className=' md:w-[70vw] shadow-md md:h-[25vh] border-[1px] rounded-[6px] border-amber-50 bg-[#ffffff] '>
+    <div className=' group hover:-translate-y-1 md:w-[70vw] shadow-md md:h-[25vh] border-[1px] rounded-[6px] border-amber-50 bg-[#ffffff] '>
       <div>
-        <div className='md:ml-5 md:mt-4 '>
-          <span className='text-md '>Data Structure And Algorizm</span>
+        <div className='md:ml-5 md:mt-4 flex justify-between'>
+          <div className='text-md '>Data Structure And Algorizm</div>
+          <div className='flex md:gap-3 md:mr-4'>
+          <Link className='md:w-7 flex justify-center md:h-7 items-center hover:bg-gray-200'> 
+            <BiSolidEdit />
+          </Link>
+          <Link className='md:w-7 flex justify-center md:h-7 items-center hover:bg-gray-200'>
+          <MdDelete />
+          </Link>
+          </div>
         </div>
         <div className='flex flex-col md:gap-2 md:ml-5 md:mt-3'>
           <span className='text-[#374151]'>DeadLine: 05-05-2025</span>
@@ -17,8 +27,9 @@ const EventCard = () => {
               <span className=' text-[#374151]'>Two weeks Remaining</span>
             </div>
             <div className=''>
-              <Link to="/login" className=' hover:ease-out hover:scale-110 transition-all delay-75 hover:border-amber-600 hover:border-2 flex justify-center items-center  md:w-[10vw] md:h-[30px] bg-[#1F2937] rounded-[3px]'>
-                <span className='text-white font-bold text-sm'>View Details</span>
+              <Link to="/courses" className='group ease-out hover:scale-110 transition-all delay-75  flex md:gap-2 justify-center items-center  md:w-[10vw] md:h-[30px] bg-[#1F2937] rounded-[3px] md:mr-7'>
+                <span className='text-white font-bold text-sm '>View Details </span>
+                <span className='text-white'><GrFormNextLink /></span>
               </Link>
             </div>
           </div>
