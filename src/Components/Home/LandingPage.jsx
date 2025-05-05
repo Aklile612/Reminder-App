871ec363f005be21fbb37b4f8ccdd37168e0b22e
Aklile Ansa <aklilemente72@gmail.com>
2025-05-05 23:56:00 +0300

    edited some margin

--- src/Components/Home/LandingPage.jsx
+++ src/Components/Home/LandingPage.jsx
@@ -4,11 +4,14 @@ import SideBar from '../SideBar/SideBar'
 
 const LandingPage = () => {
   return (
-    <div className=' flex  '>
+    <div className=' flex bg-gray-200  '>
       <div className=''>
         <SideBar/>
       </div>
-      <div className='mt-[140px] ml-[100px]'>
+      <div className='md:mt-5 md:ml-[10vw]'>
+        <span className='text-2xl font-bold '>The Calander</span>
+      </div>
+      <div className='mt-[140px] ml-[] flex md:-ml-[16vw]'>
         <EventCard/>
       </div>
     </div>
