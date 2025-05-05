f628f0786a6e7cfa5c64363f0c8ba5860cb19a54
Aklile Ansa <aklilemente72@gmail.com>
2025-05-06 01:12:24 +0300

    cleared the page

--- src/Components/Ai Assistant/AssistantPage.jsx
+++ src/Components/Ai Assistant/AssistantPage.jsx
@@ -8,11 +8,7 @@ const AssistantPage = () => {
     <>
     <Header/>
 
-    <div className='w-[90vw] h-[78vh]'>
-        <div className='md:w-[90vw] md:h-[78vh] ml-[5vw] mt-[5vh] bg-gray-500'>
-          <span>Reminder Ai</span>
-        </div>
-    </div>
+    
 
 
 
