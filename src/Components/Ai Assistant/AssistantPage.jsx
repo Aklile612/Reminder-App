8d390d865481d050b7d5b6754c1a8e4a47fbbda4
Aklile Ansa <aklilemente72@gmail.com>
2025-05-06 01:42:05 +0300

    almost done with the message

--- src/Components/Ai Assistant/AssistantPage.jsx
+++ src/Components/Ai Assistant/AssistantPage.jsx
@@ -12,8 +12,22 @@ const AssistantPage = () => {
         <SideBar/>
       </div>
       <div className='md:w-[70vw]  md:h-[78vh] rounded-[3px] md:mx-[40px] md:mt-[50px] bg-[#353f4e]'>
-        <div className='md:w-[55vw] md:my-7 rounded-[6px] md:h-[70vh] md:mx-[30px] bg-[#111827]'>
-          <span className='text-white'></span>
+        <div className='flex flex-col justify-between md:w-[55vw] md:my-7 rounded-[6px] md:h-[70vh] md:mx-[30px] bg-[#111827]'>
+          <div className='md:mt-4 md:ml-3.5'>
+            <span className='text-white'>Response</span>
+          </div>
+          <div className=''>
+            <form className='md:mb-3'>
+
+              <input 
+                  className=' w-[46vw] rounded-[6px] bg-[#374151] md:pl-2 h-10 ml-5 text-white placeholder:text-white placeholder:text-xs placeholder:font-mono' 
+                  type="text"
+                  name='question'
+                  placeholder="Type Your Question Here......"
+                  />
+            </form>
+            
+          </div>
         </div>
       </div>
     </div>
