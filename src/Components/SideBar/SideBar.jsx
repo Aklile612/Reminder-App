b43ef55ab39cc88ec86840d6a86da00de42fa678
Aklile Ansa <aklilemente72@gmail.com>
2025-05-05 22:36:41 +0300

    cleaned the shittiy side bar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -6,23 +6,7 @@ import { Link } from 'react-router-dom'
 const SideBar = () => {
   return (
     <div className='w-[300px]   h-[100vh] bg-[#221f1fe3]'>
-        <div>
-            <div className='flex flex-col md:gap-[50px] justify-center items-center md:pt-[130px]'>
-                <Link className=' flex group hover:bg-gray-600 hover:w-[200px] hover:rounded-lg hover:scale-110 hover:text-white  -ml-5 gap-3 transition-all'>
-                    <IoHomeOutline className='text-2xl   '/>
-                    <span className='text-sm font-bold font-poppins text-gray-500'>HOME</span>
-                </Link>
-                <Link to='/courses' className='group flex gap-3 transition-colors '>
-                    <FaRegFolderOpen className=' text-[26px]'/>
-                    <span className=' text-sm font-bold text-white'>COURSES</span>
-                </Link>
-
-                <Link className='group  ml-1.5 flex gap-3 transition-colors '>
-                  <FaRobot className='text-[26px]  ' />
-                  <span className=' text-sm font-bold text-white'>Ai Assistant</span>
-                </Link>                
-            </div>
-        </div>
+        
     </div>
   )
 }
