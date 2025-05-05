9f109a2bee47d4806c52b4d1891613c5610c774c
Aklile Ansa <aklilemente72@gmail.com>
2025-05-06 01:30:26 +0300

    edited the ai page

--- src/Components/Ai Assistant/AssistantPage.jsx
+++ src/Components/Ai Assistant/AssistantPage.jsx
@@ -7,7 +7,16 @@ const AssistantPage = () => {
   return (
     <>
     <Header/>
-
+    <div className='flex gap-[20px] bg-[#111827]'>
+      <div>
+        <SideBar/>
+      </div>
+      <div className='md:w-[70vw]  md:h-[78vh] rounded-[3px] md:mx-[40px] md:mt-[50px] bg-[#353f4e]'>
+        <div className='md:w-[55vw] md:my-7 rounded-[6px] md:h-[70vh] md:mx-[30px] bg-[#111827]'>
+          <span className='text-white'></span>
+        </div>
+      </div>
+    </div>
     
 
 
