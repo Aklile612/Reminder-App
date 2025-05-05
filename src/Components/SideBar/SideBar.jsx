064fb9379cbcb0e0aae504b90e32e764ca7d64cb
Aklile Ansa <aklilemente72@gmail.com>
2025-05-05 23:10:35 +0300

    added hook to access the location

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,29 +1,34 @@
 import React from 'react'
-import { FaGraduationCap ,FaRobot } from 'react-icons/fa'
-
+import { FaGraduationCap, FaRobot } from 'react-icons/fa'
 import { MdHome } from 'react-icons/md'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const SideBar = () => {
+  const location = useLocation();
+
+  const navItems = [
+    { name: 'Dashboard', icon: <MdHome className='text-2xl text-white' />, path: '/' },
+    { name: 'Courses', icon: <FaGraduationCap className='text-2xl text-white' />, path: '/courses' },
+    { name: 'AI Assistant', icon: <FaRobot className='text-2xl text-white' />, path: '/aipage' }
+  ];
+
   return (
-    <div className='w-[250px] md:mt-1.5   h-[91vh] bg-[#1F2937]'>
+    <div className='w-[250px] md:mt-1.5 h-[91vh] bg-[#1F2937]'>
       <div className='pt-5 flex flex-col md:gap-4'>
-        <div className='flex md:gap-1.5 md:ml-3 h-10 w-[200px] md:pl-4  items-center hover:bg-gray-400 rounded-[10px] group hover:scale-110'>
-          <MdHome className='text-2xl text-white '/>
-          <span className='text-white font-bold'>Dashbored</span>
-        </div>
-        <div className='flex md:gap-1.5 md:ml-3 h-10 w-[200px] md:pl-4  items-center hover:bg-gray-400 rounded-[10px] group hover:scale-110'>
-          <FaGraduationCap className='text-2xl text-white '/>
-          <span className='text-white font-bold'>Courses</span>
-        </div>
-        <div className='flex md:gap-1.5 md:ml-3 h-10 w-[200px] md:pl-4  items-center hover:bg-gray-400 rounded-[10px] group hover:scale-110'>
-          <FaRobot className='text-2xl text-white '/>
-          <span className='text-white font-bold'>Ai Assistant</span>
-        </div>
+        {navItems.map((item) => (
+          <Link
+            key={item.name}
+            to={item.path}
+            className={`flex md:gap-1.5 md:ml-3 h-10 w-[200px] md:pl-4 items-center rounded-[10px] group hover:scale-105 transition-all 
+              ${location.pathname === item.path ? 'bg-gray-400 ' : 'hover:bg-gray-600'}`}
+          >
+            {item.icon}
+            <span className='text-white font-bold'>{item.name}</span>
+          </Link>
+        ))}
       </div>
-        
     </div>
   )
 }
 
-export default SideBar
+export default SideBar;
