367439b7d4cb7ba0c46fe38359265350f635e59d
Aklile Ansa <aklilemente72@gmail.com>
2025-05-05 23:23:11 +0300

    jusst a little edit

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -13,14 +13,14 @@ const SideBar = () => {
   ];
 
   return (
-    <div className='w-[250px] md:mt-1.5 h-[91vh] bg-[#1F2937]'>
-      <div className='pt-5 flex flex-col md:gap-4'>
+    <div className='w-[250px] md:mt-1.5 h-[91vh] rounded-[5px] bg-[#1F2937]'>
+      <div className='pt-10 flex flex-col md:gap-4'>
         {navItems.map((item) => (
           <Link
             key={item.name}
             to={item.path}
-            className={`flex md:gap-1.5 md:ml-3 h-10 w-[200px] md:pl-4 items-center rounded-[10px] group hover:scale-105 transition-all 
-              ${location.pathname === item.path ? 'bg-gray-400 ' : 'hover:bg-gray-600'}`}
+            className={`flex md:gap-1.5 md:ml-3 h-10 w-[200px] md:pl-4 items-center rounded-r-[10px] rounded-t-[10px] rounded-b-[10px]  group hover:scale-105 transition-all 
+              ${location.pathname === item.path ? 'bg-gray-400 border-l-4 h-2 border-black' : 'hover:bg-gray-600'}`}
           >
             {item.icon}
             <span className='text-white font-bold'>{item.name}</span>
