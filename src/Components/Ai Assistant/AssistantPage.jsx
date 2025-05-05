72af725ba60888cf18d107a28d88708d2f67d87f
Aklile Ansa <aklilemente72@gmail.com>
2025-05-05 23:09:47 +0300

    tried to edit

--- src/Components/Ai Assistant/AssistantPage.jsx
+++ src/Components/Ai Assistant/AssistantPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import SideBar from '../SideBar/SideBar'
 
 const AssistantPage = () => {
   return (
