d0585cebb771437a0b73bcaec70bf95bdf6f8cee
Aklile Ansa <aklilemente72@gmail.com>
2025-05-06 00:46:35 +0300

    stated the page

--- src/Components/Right Side Bar/RightSideBar.jsx
+++ src/Components/Right Side Bar/RightSideBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightSideBar = () => {
   return (
-    <div className=''>
+    <div className='w-[250px] md:mt-1.5 h-[91vh] rounded-[5px] bg-[#1F2937]'>
       
     </div>
   )
