be982115450d3bce6a15725cf17876c57c28cb1f
Aklile Ansa <aklilemente72@gmail.com>
2025-05-06 01:50:04 +0300

    mostly done the ai page

--- src/Components/Ai Assistant/AssistantPage.jsx
+++ src/Components/Ai Assistant/AssistantPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import SideBar from '../SideBar/SideBar'
+import { IoIosSend } from 'react-icons/io'
+import { FaRobot } from 'react-icons/fa'
 
 const AssistantPage = () => {
   return (
@@ -12,11 +14,15 @@ const AssistantPage = () => {
         <SideBar/>
       </div>
       <div className='md:w-[70vw]  md:h-[78vh] rounded-[3px] md:mx-[40px] md:mt-[50px] bg-[#353f4e]'>
-        <div className='flex flex-col justify-between md:w-[55vw] md:my-7 rounded-[6px] md:h-[70vh] md:mx-[30px] bg-[#111827]'>
+        <div className='flex gap-4 md:ml-3'>
+        <FaRobot className='text-[20px] text-white'/>
+        <span className='text-white font-serif'>Reminder Ai</span>
+        </div>
+        <div className='flex flex-col justify-between md:w-[55vw] md:my-4 rounded-[6px] md:h-[70vh] md:mx-[30px] bg-[#111827]'>
           <div className='md:mt-4 md:ml-3.5'>
             <span className='text-white'>Response</span>
           </div>
-          <div className=''>
+          <div className='flex md:gap-4'>
             <form className='md:mb-3'>
 
               <input 
@@ -26,7 +32,9 @@ const AssistantPage = () => {
                   placeholder="Type Your Question Here......"
                   />
             </form>
-            
+            <div className='text-3xl flex items-center justify-center rounded-[7px] text-white md:w-[40px] md:h-[40px]  bg-[#374151]'>
+            <IoIosSend />
+            </div>
           </div>
         </div>
       </div>
