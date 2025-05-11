7933defafbcddf63aa42413e0581570a4a289707
Aklile Ansa <aklilemente72@gmail.com>
2025-05-11 20:20:56 +0300

    added the cors issue

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,10 @@ import express from "express"
 import authRoute from "./Routes/authRoute.js"
 import courseRoute from "./Routes/courseRoute.js"
 import calanderRoute from "./Routes/calanderRoute.js"
-
+import cors from 'cors'
+ 
 const app=express()
-
+app.use(cors()); 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
