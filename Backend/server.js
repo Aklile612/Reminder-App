40dae939a2ecf2f34da6704914452d6d35c1ed43
Aklile Ansa <aklilemente72@gmail.com>
2025-05-11 20:20:37 +0300

    edited the surver page

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,14 @@ import express from "express"
 import dotenv from 'dotenv'
 import dbconnect from "./db/dbConnection.js"
 import app from "./app.js"
+import cors from 'cors'
 
+app.use(cors()); 
 app.get('/',(req,res)=>{
     res.send("hello")
 })
 
-app.listen(3000,(req,res)=>{
+app.listen(5000,(req,res)=>{
     dbconnect()
-    console.log("app running on port 3000")
+    console.log("app running on port 5000")
 })
