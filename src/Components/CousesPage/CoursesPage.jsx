0b33b8f43c01ef0db94a084bd80bdf00481a74ad
Aklile Ansa <aklilemente72@gmail.com>
2025-05-14 05:19:50 +0300

    edited the course page

--- src/Components/CousesPage/CoursesPage.jsx
+++ src/Components/CousesPage/CoursesPage.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { FaAngleDown } from "react-icons/fa6";
 import { FaPlus } from "react-icons/fa6";
-
+import axios from '../../../axiosBase'
 
 
 
 const CoursesPage = () => {
-  const [elecLink,setelectLink]=useState(false)
+  const [link,setLink]=useState(false)
   const [informationLink,setinformationLink]=useState(false)
   const [openCourseIndex, setOpenCourseIndex] = useState(null)
   const [selectedcourse,setselectedcourse]=useState(null)
   const [selectedcalander,setselectedcalander]=useState(null)
+  const [departemtName,setdepartemntName]=useState([])
+  const [openDepartmentId, setOpenDepartmentId] = useState(null);
+  const [departmentCourses,setdepartmentCourses]=useState({})
   const courseName=[
     {title:"Data structure and Algor.."},
     {title:"Advanced DataBase"},
@@ -35,6 +38,32 @@ const CoursesPage = () => {
     {name:"Mid Exam"},
     {name:"Final Exam"},
   ]
+  const getDepartemnt=async()=>{
+    try {
+      const {data}=await axios.get('/course/finddepartment')
+      
+      setdepartemntName(data.department)
+      console.log(data.department)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const getDepartemntCourses=async (department_id)=>{
+    try {
+      const {data}=await axios.get(`/course/findcourse/${department_id}`)
+      setdepartmentCourses(data.courses);
+      setOpenDepartmentId(department_id);
+      setLink(!(link));
+      console.log(data.courses)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    getDepartemnt();
+    
+  },[])
   // console.log(selectedcourse,selectedcalander.name)
   return (
     <>
@@ -49,92 +78,31 @@ const CoursesPage = () => {
               <div className='flex items-center md:mt-11 justify-center md:w-[150px] md:h-[50px] rounded-2xl hover:shadow-amber-500 hover:shadow-sm hover:-translate-y-1 bg-gray-300'>
                 <span className='font-bold'>Course Names</span>
               </div>
-              <div className='md:w-[290px] md:h-[50px] bg-gray-400 items-center flex justify-between   '>
-                <span className='md:ml-2'>Information Science</span>
-                <div onClick={()=>{
-                  setinformationLink(!informationLink)
-                  if (elecLink)
-                  setelectLink(!elecLink)
-                }}  className='md:mr-2'><FaAngleDown/></div>
-              </div>
               <div className='flex flex-col'>
 
-                {informationLink && (
-                  
-                  <div className='flex flex-col gap-1 md:-mt-6'>
-                    {courseName.map((course,index)=>(
-                      
-                      <div className='flex flex-col '  >
+              <div className="flex flex-col space-y-2">
+                {departemtName.map((department, index) => (
+                  <div
+                    key={index}
+                    className="md:w-[290px] md:h-[50px] bg-gray-400 flex items-center justify-between rounded-md shadow"
+                  >
+                    <span className="text-white font-semibold">{department.name}</span>
+                    <div className='md:mr-2 ' onClick={()=>{
+                      getDepartemntCourses(department._id)
+                    }}><FaAngleDown/></div>
 
-                        <div key={index} className='md:w-[230px] md:h-[30px] bg-amber-50 font-bold  flex  items-center  justify-between' >{course.title} <div onClick={() => setOpenCourseIndex(openCourseIndex === index ? null : index)
-                          
-                        }
-                        ><FaAngleDown/></div >
-
-                        </div>
-                        <div className='flex justify-center '>
-                        {openCourseIndex === index && (
-                          <div className="flex flex-col gap-1">
-                            {acadamictimes.map((acadamic, subIndex) => (
-                              <div className='md:w-[190px] md:h-[30px] bg-gray-500 font-bold  flex  items-center text-white justify-between' key={subIndex}>{acadamic.name} <div 
-                              onClick={()=>{setselectedcalander(acadamic);
-                                setselectedcourse(course.title)
-                              }}><FaPlus/></div> 
-                              </div>
-                              
-                            
-                            ))}
+                    {link && (
+                      <div>
+                        {departmentCourses.map((course,index)=>(
+                          <div>
+                            <span>{course.name}</span>
                           </div>
-                        )}
-                        </div>
+                        ))}
                       </div>
-                      
-                      
-                    ))}
+                    )}
                   </div>
-                
-                )}
+                ))}
               </div>
-              <div className='md:w-[290px] md:h-[50px] bg-gray-400 items-center flex justify-between   '>
-                <span className='md:ml-2'>Electrical Engineering</span>
-                <div onClick={()=>{
-                  setelectLink(!(elecLink))
-                  if (informationLink)
-                  setinformationLink(!(informationLink))
-                }} className='md:mr-2'><FaAngleDown /></div>
-              </div>
-              <div className='flex flex-col'>
-                {elecLink && (
-                  <div className='flex flex-col gap-1 md:-mt-6'>
-                    {courseName1.map((course,index)=>(
-                      
-                      <div className='flex flex-col '  >
-
-                        <div key={index} className='md:w-[230px] md:h-[30px] bg-amber-50 font-bold  flex  items-center  justify-between' >{course.title} <div onClick={() => setOpenCourseIndex(openCourseIndex === index ? null : index)}
-                        ><FaAngleDown/></div >
-
-                        </div>
-                        <div className='flex justify-center '>
-                        {openCourseIndex === index && (
-                          <div className="flex flex-col gap-1">
-                            {acadamictimes.map((acadamic, subIndex) => (
-                              <div className='md:w-[190px] md:h-[30px] bg-gray-500 font-bold  flex  items-center text-white justify-between' key={subIndex}>{acadamic.name} <div onClick={()=>{setselectedcalander(acadamic);
-                                setselectedcourse(course.title)
-                              }}><FaPlus/></div> 
-                              </div>
-                              
-                            
-                            ))}
-                          </div>
-                        )}
-                        </div>
-                      </div>
-                      
-                      
-                    ))}
-                  </div>
-
-                )}
               </div>
             </div>
           </div>
