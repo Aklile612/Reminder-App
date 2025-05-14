3c74f8f86bb28086213e22786450a2fcc4d4225f
Aklile Ansa <aklilemente72@gmail.com>
2025-05-14 07:16:31 +0300

    edited colour

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ import Footer from './Components/Footer/Footer'
 const Home = () => {
   return (
     <>
-     <div className="flex flex-col min-h-screen">
+     <div className="flex bg-gray-200 flex-col min-h-screen">
     <Header />
     <main className="flex-grow">
       <LandingPage/>
