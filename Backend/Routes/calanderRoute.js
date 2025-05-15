6849cfab28dedd3c7551e20c3a4d7482c6bafcd2
Aklile Ansa <aklilemente72@gmail.com>
2025-05-15 18:26:22 +0300

    edited

--- Backend/Routes/calanderRoute.js
+++ Backend/Routes/calanderRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addCalander, allEventByCoursId, allevents, singleEventByCalander } from "../controller/calanderController.js";
+import { addCalander, allEventByCoursId, allevents, singleEventByCalander,deleteCalander } from "../controller/calanderController.js";
 
 
 const router= Router()
@@ -8,5 +8,5 @@ router.post("/addcalander/:course_id",addCalander);
 router.get("/allcalanders",allevents)
 router.get("/singlecalanderdetail/:calander_id",singleEventByCalander)
 router.get("/singlecoursecalander/:course_id",allEventByCoursId)
-
+router.delete("/singlecalanderDelete/:calander_id",deleteCalander)
 export default router
