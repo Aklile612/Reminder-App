6f4126873be6bae215f1559cc461c36eb12cd282
Aklile Ansa <aklilemente72@gmail.com>
2025-05-15 05:43:59 +0300

    still not finished

--- src/Components/CousesPage/CoursesPage.jsx
+++ src/Components/CousesPage/CoursesPage.jsx
@@ -6,6 +6,7 @@ import axios from '../../../axiosBase';
 import SideBar from '../SideBar/SideBar';
 import RightSideBar from '../Right Side Bar/RightSideBar';
 import { FaClock } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const CoursesPage = () => {
   const [departments, setDepartments] = useState([]);
@@ -14,6 +15,11 @@ const CoursesPage = () => {
   const [selectedCourse, setSelectedCourse] = useState(null);
   const [selectedCalendar, setSelectedCalendar] = useState(null);
   const [calander,setcalander]=useState(false)
+  const [formData,setformData]=useState({
+      "username":"",
+      "email":"",
+      "password":""
+    })
 
   const academicTimes = [
     { name: "Quiz" },
@@ -110,20 +116,20 @@ const CoursesPage = () => {
             {selectedCourse && (
               <div className="flex-1 flex justify-center items-center">
                 <div className="space-y-4 flex-col">
-                  <div className='md:w-[40vw] md:h-[8vh] bg-[#ffffff] rounded-[10px] flex '>
+                  <div className='md:w-[40vw] md:h-[10vh] bg-[#ffffff] rounded-[10px] flex items-center '>
                     <h3 className=" md:ml-3 text-center text-black font-bold text-3xl font-serif">
                       {selectedCourse}
                     </h3>
                   </div>
-                  <div className='md:w-[40vw] md:h-[50vh] flex flex-col gap-3 bg-[#ffffff]'>
+                  <div className='md:w-[40vw] md:h-[65vh] md:py-4 flex flex-col gap-3 bg-[#ffffff] rounded-[10px]'>
                     <div className='flex flex-col'>
-                      <span className='text-black md:ml-5 font-normal  text-[20px]'>Create Reminder</span>
+                      <span className='text-black md:ml-5 font-normal  text-[24px]'>Create Reminder</span>
                     </div>
-                      <span className="text-black md:ml-6 ">
+                      <span className="text-black text-[20px] md:ml-6 ">
                         Topic
                       </span>
                     
-                  <form className="flex flex-col items-center justify-center">
+                  <form className="flex flex-col md:gap-3 items-center justify-center">
                     <div>
                       <input
                         type="text"
@@ -131,14 +137,41 @@ const CoursesPage = () => {
                         className="md:w-[35vw] md:-ml-5 py-2 rounded-[5px] border border-gray-300 bg-transparent text-black placeholder-[#b3adad] placeholder:pl-2.5 placeholder:font-bold"
                         />
                     </div>
-                    <div className='md:w-[10vw]'>
-                    
-                    <span>Date</span>
-                    <input
-                      type="time"
-                      className='w- h-10 border flex justify-between border-black rounded px-3 bg-white text-black'
-                    />  
+                    <div className='flex  gap-4'>
+
+                      <div className=''>
+                        <span>Time</span>
+                        <input
+                          type="time"
+                          className='w-[10vw] h-10 border flex gap-[15vw] placeholder:bg-gray-300 justify-between border-black rounded   px-3 bg-white text-black'
+                          />  
+                      </div>
+                      <div className=''>
+                      
+                        <span>Date</span>
+                        <input
+                          type="date"
+                          className='w-[15vw] h-10 border flex justify-between border-black rounded px-3 bg-white  text-black'
+                          />  
+                      </div>
                     </div>
+                    <div className="flex flex-col">
+                      <label className="mb-1 text-black font-medium text-center">Calendar Type</label>
+                      <select
+                        className="w-[18vw] h-10 px-3 border border-black rounded bg-gray-200 text-black"
+                        defaultValue=""
+                      >
+                        <option value="" disabled>Select type</option>
+                        <option value="Quiz">Quiz</option>
+                        <option value="Assignment">Assignment</option>
+                        <option value="Mid Exam">Mid Exam</option>
+                        <option value="Final Exam">Final Exam</option>
+                      </select>
+                    </div>
+                    <button  className=' hover:ease-out hover:scale-110 transition-all delay-75  hover:border-2 flex justify-center items-center  md:w-[15vw] md:h-[50px] md:mt-[2vh] bg-[#1F2937] rounded-[6px]'>
+                        <span className='text-white font-bold text-sm'>Set Reminder</span>
+                    </button>
+
                   </form>
                   
                   </div>
