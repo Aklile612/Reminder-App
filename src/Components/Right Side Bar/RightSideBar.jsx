42b7d29e5e9eba099caa7520b118fa361ba83a75
Aklile Ansa <aklilemente72@gmail.com>
2025-05-15 17:12:43 +0300

    edited

--- src/Components/Right Side Bar/RightSideBar.jsx
+++ src/Components/Right Side Bar/RightSideBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const RightSideBar = () => {
+const RightSideBar = ({len}) => {
+  // console.log(len)
   return (
-    <div className='w-[250px] md:mt-1.5 h-[91vh] rounded-[5px] py-7 bg-[#1F2937]'>
+    <div className='w-[250px] md:mt-1.5 h-full rounded-[5px] py-7 bg-[#1F2937]'>
       <div className='bg-gray-600 flex flex-col gap-2 items-center w-[200px] md:h-[150px] rounded-[3px] md:mt-5 md:ml-6'>
         <span className='text-white font-mono text-lg '>Course Statstics</span>
         <div className='flex md:w-[180px] text-white font-semibold justify-between'>
           <span>Total Courses</span>
-          <span>12</span>
+          <span>{len}</span>
         </div>
         <div className='flex md:w-[180px] text-white font-semibold justify-between'>
           <span>Past Deadlines</span>
