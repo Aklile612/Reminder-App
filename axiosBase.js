6feaaa7d62d6892dc07d8acf578ec2b9dbb8fff1
Aklile Ansa <aklilemente72@gmail.com>
2025-05-19 23:33:42 +0300

    edited

--- axiosBase.js
+++ axiosBase.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosBase=axios.create({
-    baseURL:'http://localhost:5000'
+    baseURL:'https://reminder-app-backend-oldu.onrender.com'
 })
 
 export default axiosBase;
